ff8cf2ea3f92bf882cc111e9c9dac4b0647b2400
Eren Aslan <91415630+Ezoras@users.noreply.github.com>
2024-11-02 11:33:12 +0300

    Add or update file code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # CS 421 Programming Assignment 1 - PseudoGit
 
 def greet_the_student():
-    return "22102329"
+    return "22102777"
 
 if(__name__ == "__main__"):
     print(greet_the_student())
